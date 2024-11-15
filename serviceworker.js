bb37bea1db72c47f309e742acf668ba2e80f0b2d
TheHDLabs <thehdlabs@gmail.com>
2024-11-15 18:01:24 +0530

    no message

--- serviceworker.js
+++ serviceworker.js
@@ -2,6 +2,8 @@ const staticDevCoffee = "dev-coffee-site-v1";
 const assets = [
   "/",
   "/index.html",
+  "/pages/aboutus.html",
+  "/pages/contactus.html",
   "/css/style.css",
   "/js/app.js",
   "/images/coffee1.jpg",
@@ -12,20 +14,20 @@ const assets = [
   "/images/coffee6.jpg",
   "/images/coffee7.jpg",
   "/images/coffee8.jpg",
-  "/images/coffee9.jpg"
+  "/images/coffee9.jpg",
 ];
 
-self.addEventListener("install", installEvent => {
+self.addEventListener("install", (installEvent) => {
   installEvent.waitUntil(
-    caches.open(staticDevCoffee).then(cache => {
+    caches.open(staticDevCoffee).then((cache) => {
       cache.addAll(assets);
     })
   );
 });
 
-self.addEventListener("fetch", fetchEvent => {
+self.addEventListener("fetch", (fetchEvent) => {
   fetchEvent.respondWith(
-    caches.match(fetchEvent.request).then(res => {
+    caches.match(fetchEvent.request).then((res) => {
       return res || fetch(fetchEvent.request);
     })
   );
